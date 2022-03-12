a1488c09586e632bef5ec623e11d38f901078015
Ben Monaghan <monoganog@gmail.com>
2022-03-13 05:58:26 +1100

    Import fonts and begin extensions

--- Assets/Scripts/PhysicalCameraTexture.cs
+++ Assets/Scripts/PhysicalCameraTexture.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PhysicalCameraTexture : MonoBehaviour
 {
-
+    public Text cameraName;
     public Material cubeMat;
     private WebCamTexture webcam;
-
+    private WebCamTexture webCamTexture;
     // Start is called before the first frame update
     void Start()
     {
-        webcam = new WebCamTexture();
+        webcam = new WebCamTexture(Screen.width, Screen.height, 60);
         cubeMat.mainTexture = webcam;
         webcam.Play();
     }
@@ -19,6 +20,14 @@ public class PhysicalCameraTexture : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.transform.rotation *= Quaternion.AngleAxis(30 * Time.deltaTime, new Vector3(1, 2, 3) * 10);
+        //this.transform.rotation *= Quaternion.AngleAxis(30 * Time.deltaTime, new Vector3(1, 2, 3) * 10);
+    }
+
+    private void ShowCameras()
+    {
+        foreach (WebCamDevice d in WebCamTexture.devices)
+        {
+            cameraName.text += d.name + (d.name == webCamTexture.deviceName ? "*" : "" + "\n");
+        }
     }
 }
---
+++ Assets/Scripts/WebcamTest.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+
+public class WebcamTest : MonoBehaviour
+{
+    public string deviceName;
+    WebCamTexture wct;
+    public Material cubeMat;
+
+    // Use this for initialization
+    void Start()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        deviceName = devices[0].name;
+        wct = new WebCamTexture(deviceName, 400, 300, 12);
+        cubeMat.mainTexture = wct;
+        wct.Play();
+    }
+
+    // For photo varibles
+
+    public Texture2D heightmap;
+    public Vector3 size = new Vector3(100, 10, 100);
+
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 70, 50, 30), "Click"))
+            TakeSnapshot();
+
+    }
+
+    // For saving to the _savepath
+    private string _SavePath = "C:/Users/monog/Documents/Projects/SIT383-FirstProject/Assets/Screenshots/Pic"; //Change the path here!
+    int _CaptureCounter = 0;
+
+    void TakeSnapshot()
+    {
+        Texture2D snap = new Texture2D(wct.width, wct.height);
+        snap.SetPixels(wct.GetPixels());
+        snap.Apply();
+
+        Debug.Log("here");
+        System.IO.File.WriteAllBytes(_SavePath + _CaptureCounter.ToString() + ".png", snap.EncodeToPNG());
+        ++_CaptureCounter;
+    }
+}
